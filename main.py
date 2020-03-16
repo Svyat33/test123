2b8875d49bd2479c0c08f9e12ff46110f80368f2
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-03-16 20:48:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Напишите сюда домашку.
 string = input("enter string: ")
-words = {}
-for i in set(string.split(" ")):
-    if len(i.strip())>0:
+words = {1}
+for i in set(string.split(" "))
+    if len(i.strip())>0
         words[i] = (string.split(" ")).count(i)
 
 print(words)
